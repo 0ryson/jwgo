1ca53690b946892b17d7d835ebdabab0d077fb49
David Merino <david.merino.developer@gmail.com>
2023-10-01 10:09:21 +0200

    Add adapter Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [
     react({
       //experimentalReactChildren: true,
-    }),
-    tailwind()
+    }), tailwind()
   ],
-  output: 'server',
-  adapter: render()
+  output: 'hybrid',
+  adapter: vercel()
 });
